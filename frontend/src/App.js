782cbe912dc6f7835a949f6706f847ccbb0077da
Fatih Yazıcı <fatihyazici67@gmail.com>
2019-08-06 08:48:55 +0300

    alert input implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import AlertInput from "./components/AlertInput";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <AlertInput />
+          </Col>
+          <Col>2</Col>
+        </Row>
+        <Row>
+          <Col>3</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ frontend/src/components/AlertInput.js
@@ -0,0 +1,84 @@
+import React, {Component} from "react"
+import {Form, Row, Col, Button} from "react-bootstrap"
+import axios from "axios"
+
+class AlertInput extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            alertName: "", 
+            alertURL: "",
+            httpMethod: "",
+            controlPeriod: 1
+        };
+    }
+
+    handleAlertNameChange = (event) => {
+        this.setState({alertName: event.target.value});
+        console.log(event.target.value);
+    }
+
+    handleAlertURLChange = (event) => {
+        this.setState({alertURL: event.target.value});
+        console.log(event.target.value);
+    }
+
+    handleHTTPMethodChange = (event) => {
+        this.setState({httpMethod: event.target.value});
+        console.log(event.target.value);
+    }
+
+    handleControlPeriodChange = (event) => {
+        this.setState({controlPeriod: event.target.value});
+        console.log(event.target.value);
+    }
+
+    handleAlertSave = () => {
+        axios.post("http://localhost:8090/alert", {
+            name: this.state.alertName,
+            url: this.state.alertURL,
+            method: this.state.httpMethod,
+            period: this.state.controlPeriod
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Form className="AlertInputBox">
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={4}>Adı:</Form.Label>
+                        <Col>
+                            <Form.Control type="text" value={this.state.alertName} onChange={this.handleAlertNameChange}/>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={4}>URL:</Form.Label>
+                        <Col>
+                            <Form.Control type="text" value={this.state.alertURL} onChange={this.handleAlertURLChange}/>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={4}>HTTP Metodu:</Form.Label>
+                        <Col>
+                            <Form.Control as="select" value={this.state.httpMethod} onChange={this.handleHTTPMethodChange}>
+                                <option>GET</option>
+                                <option>POST</option>
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={4}>Kontrol Periyodu:</Form.Label>
+                        <Col>
+                            <Form.Control type="number" min="1" value={this.state.controlPeriod} onChange={this.handleControlPeriodChange} />
+                        </Col>
+                    </Form.Group>
+                    <Button variant="primary" size="lg" block onClick={this.handleAlertSave}>Kaydet</Button>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default AlertInput;
